cccc9866d03f8040b3e89af73dc95314698418c7
ForestTschirhart <77809401+ForestTschirhart@users.noreply.github.com>
2021-05-20 15:07:08 -0700

    hopefully this works
    test

--- time_dilation/static/script.js
+++ time_dilation/static/script.js
@@ -2,9 +2,18 @@ var spaceship = document.getElementById("spaceship");
 var photon = document.getElementById("photon");
 var photon2 = document.getElementById("photon2");
 function startAnimation(){
+<<<<<<< Updated upstream
    spaceship.classList.add("animateSpaceship");
    photon.classList.add("animatePhoton");
 }
 function startAnimation2(){
     photon2.classList.add("animatePhoton");
  }
+=======
+    spaceship.classList.add("animateSpaceship");
+    photon.classList.add("animatePhoton");
+}
+function startAnimation2(){
+    photon2.classList.add("animatePhoton");
+} 
+>>>>>>> Stashed changes
