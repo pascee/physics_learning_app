d4743978ff9c4d460183986ebb6aec0ea2934808
ElekTToth <78376027+ElekTToth@users.noreply.github.com>
2021-05-20 14:49:39 -0700

    Revert "Merge branch 'main' of https://github.com/pascee/physics_learning_app into main"
    This reverts commit b20d58b84fc7544295c69db2da3913e00a59bf92, reversing
    changes made to 6c35b4232be2564d29ad6c9518a569280ffdb61b.

--- time_dilation/static/script.js
+++ time_dilation/static/script.js
@@ -1,10 +1,4 @@
 var spaceship = document.getElementById("spaceship");
-var photon = document.getElementById("photon");
-var photon2 = document.getElementById("photon2");
 function startAnimation(){
-   spaceship.classList.add("animateSpaceship");
-   photon.classList.add("animatePhoton");
+    spaceship.classList.add("animate");
 }
-function startAnimation2(){
-    photon2.classList.add("animatePhoton");
- }
